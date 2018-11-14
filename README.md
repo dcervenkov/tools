880fde7eccfe2410dbaf5a073e84b5a8b0a4b4f3
Daniel Cervenkov <d.cervenkov@gmail.com>
2018-11-14 16:57:03 +0100

    Add monitor-process.sh script

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ A small script to find and move pics not used in a LaTeX file.
 #### create_image_slides.py
 
 Script that creates LaTeX slides with all supplied images.
+
+## Shell
+
+#### monitor-process.sh
+
+Send email to predefined address after a process exits.
---
+++ shell/monitor-process.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "ERROR: Must have 1 argument!"
+	echo "Usage: $0 PROCESS-NAME"
+	exit 1
+fi
+
+if [ ! -f $HOME/.mailaddress ]; then
+	echo "ERROR: Can't find file '$HOME/.mailaddress', please"\
+	     "create it and populate it with the email address"\
+	     "to which notifications should be sent."
+	exit 2
+fi
+
+PROC_NAME=$1
+EMAIL=$(cat $HOME/.mailaddress)
+
+echo "Monitoring process '$PROC_NAME'."
+echo "Will send email to $EMAIL when the process exits."
+
+FOUND=false
+START=$(date +%s)
+
+while true; do
+	if pgrep -x $PROC_NAME > /dev/null; then
+		FOUND=true
+		sleep 6
+	else
+		if [ "$FOUND" = false ]; then
+			echo "Process '$PROC_NAME' not found."
+			exit 0
+		fi
+		echo "Process '$PROC_NAME' not found anymore."
+
+		END=$(date +%s)
+		HOURS=$(echo "scale=0; ($END - $START) / 3600" | bc -l )
+		MINUTES=$(echo "scale=0; (($END - $START) % 3600)/60" | bc -l )
+
+		(echo "Monitored process '$PROC_NAME' could no longer be found at $(date) after approximately $HOURS hours and $MINUTES minutes." | mail -s "$PROC_NAME Finished" $EMAIL) && echo "Mail sent."
+
+		exit 0
+	fi
+done
