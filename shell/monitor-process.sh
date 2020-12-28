eeca0238e05ee5600485d95bdae2a932cef59f4c
Daniel Cervenkov <d.cervenkov@gmail.com>
2020-12-28 10:16:42 +0000

    Change indent to spaces in monitor-process

--- shell/monitor-process.sh
+++ shell/monitor-process.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-	echo "ERROR: Must have 1 argument!"
-	echo "Usage: $0 PROCESS-NAME"
-	exit 1
+    echo "ERROR: Must have 1 argument!"
+    echo "Usage: $0 PROCESS-NAME"
+    exit 1
 fi
 
 if [ ! -f $HOME/.mailaddress ]; then
-	echo "ERROR: Can't find file '$HOME/.mailaddress', please"\
-	     "create it and populate it with the email address"\
-	     "to which notifications should be sent."
-	exit 2
+    echo "ERROR: Can't find file '$HOME/.mailaddress', please"\
+         "create it and populate it with the email address"\
+         "to which notifications should be sent."
+    exit 2
 fi
 
 PROC_NAME=$1
@@ -23,22 +23,22 @@ FOUND=false
 START=$(date +%s)
 
 while true; do
-	if pgrep -x -u $USER $PROC_NAME > /dev/null; then
-		FOUND=true
-		sleep 6
-	else
-		if [ "$FOUND" = false ]; then
-			echo "[$(date '+%F %R')]" "Process '$PROC_NAME' not found."
-			exit 0
-		fi
-		echo "[$(date '+%F %R')]" "Process '$PROC_NAME' not found anymore."
+    if pgrep -x -u $USER $PROC_NAME > /dev/null; then
+        FOUND=true
+        sleep 6
+    else
+        if [ "$FOUND" = false ]; then
+            echo "[$(date '+%F %R')]" "Process '$PROC_NAME' not found."
+            exit 0
+        fi
+        echo "[$(date '+%F %R')]" "Process '$PROC_NAME' not found anymore."
 
-		END=$(date +%s)
-		HOURS=$(echo "scale=0; ($END - $START) / 3600" | bc -l )
-		MINUTES=$(echo "scale=0; (($END - $START) % 3600)/60" | bc -l )
+        END=$(date +%s)
+        HOURS=$(echo "scale=0; ($END - $START) / 3600" | bc -l )
+        MINUTES=$(echo "scale=0; (($END - $START) % 3600)/60" | bc -l )
 
-		(echo "Monitored process '$PROC_NAME' could no longer be found at $(date) after approximately $HOURS hours and $MINUTES minutes." | mail -s "$PROC_NAME Finished" $EMAIL) && echo "Mail sent."
+        (echo "Monitored process '$PROC_NAME' could no longer be found at $(date) after approximately $HOURS hours and $MINUTES minutes." | mail -s "$PROC_NAME Finished" $EMAIL) && echo "Mail sent."
 
-		exit 0
-	fi
+        exit 0
+    fi
 done
