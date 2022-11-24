cbf423bbac267485c62fdf5d5662b13f698d575f
Daniel Cervenkov <d.cervenkov@gmail.com>
2022-11-24 16:27:29 +0000

    Allow monitoring of files & increase sensitivity
    Switch from pages to bytes for higher granularity.

--- shell/monitor-size.sh
+++ shell/monitor-size.sh
@@ -17,7 +17,7 @@ shift $((OPTIND-1))
 
 if [ "$#" -ne 1 ]; then
     echo "ERROR: Must have 1 argument!"
-    echo "Usage: $0 [OPTIONS] DIRECTORY"
+    echo "Usage: $0 [OPTIONS] PATH"
     exit 1
 fi
 
@@ -29,13 +29,13 @@ if [ ! -f "$HOME/.mailaddress" ] && [ -z "$EMAIL" ]; then
     exit 2
 fi
 
-DIRECTORY=$(realpath "$1")
+TARGET=$(realpath "$1")
 
 if [ -z "$EMAIL" ]; then
     EMAIL=$(cat "$HOME/.mailaddress")
 fi
 
-if ! du -s "${DIRECTORY}"; then
+if ! du -bs "${TARGET}"; then
     exit 3
 fi
 
@@ -43,25 +43,26 @@ if [ -z "$SLEEP_INTERVAL" ]; then
     SLEEP_INTERVAL=6
 fi
 
-echo "[$(date '+%F %R')]" "Monitoring the size of '$DIRECTORY' directory every ${SLEEP_INTERVAL}s."
-echo "[$(date '+%F %R')]" "Email will be sent to $EMAIL when the directory stops growing."
+echo "[$(date '+%F %R')]" "Monitoring the size of '$TARGET' every ${SLEEP_INTERVAL}s."
+echo "[$(date '+%F %R')]" "Email will be sent to $EMAIL when the target stops growing."
+fflush
 
 LAST_SIZE=0
 START=$(date +%s)
 
 while true; do
-    SIZE=$(du -s "${DIRECTORY}" | cut -f1)
+    SIZE=$(du -bs "${TARGET}" | cut -f1)
     if [[ $SIZE -gt $LAST_SIZE ]] ; then
         LAST_SIZE=$SIZE
         sleep $SLEEP_INTERVAL
     else
-        echo "[$(date '+%F %R')]" "Size of '$DIRECTORY' didn't increase in the last $SLEEP_INTERVAL seconds."
+        echo "[$(date '+%F %R')]" "Size of '$TARGET' didn't increase in the last $SLEEP_INTERVAL seconds."
 
         END=$(date +%s)
         HOURS=$(echo "scale=0; ($END - $START) / 3600" | bc -l )
         MINUTES=$(echo "scale=0; (($END - $START) % 3600)/60" | bc -l )
 
-        (echo "Monitored directory '$DIRECTORY' stopped growing at $(date) after approximately $HOURS hours and $MINUTES minutes." | mail -s "$DIRECTORY Stopped Growing" "$EMAIL") && echo "Mail sent."
+        (echo "Monitored target '$TARGET' stopped growing at $(date) after approximately $HOURS hours and $MINUTES minutes." | mail -s "$TARGET Stopped Growing" "$EMAIL") && echo "Mail sent."
 
         exit 0
     fi
